106618c74b7639b822b88ab9b9534af5d7c7fd74
nishizawa <nishizawa@rubyquitous.co.jp>
2014-07-17 18:15:42 +0900

    The functions for making posts are mounted.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿を保存しました"
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+def update
+  @post = Post.find(params[:id])
+if @post.update_attributes(post_params)
+  redirect_to posts_path, notice: '更新されましたよ'
+else
+  render action: 'edit'
+end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end
+
+
+  private
+  def post_params
+    params.require(:post).permit(
+      :title, :content)
+  end
+end
