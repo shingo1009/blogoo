d9053d584b1830f008431eb3f4999949ac7af24b
Nishizawa <nishizawa@rubyquitous.co.jp>
2014-09-07 21:20:53 +0900

    topページのrootを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root :to => 'posts#index'
+   root :to => 'top#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
